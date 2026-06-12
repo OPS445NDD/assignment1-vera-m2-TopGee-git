270e5acd80d785b9da000985d0a7503050b71569
Prince Arkoh <aparkoh@myseneca.ca>
2026-06-12 10:27:14 -0400

    added milestone2 script

--- assignment1.py
+++ assignment1.py
@@ -28,8 +28,24 @@ def day_of_week(year: int, month: int, date: int) -> str:
 
 
 def mon_max(month:int, year:int) -> int:
-    "returns the maximum day for a given month. Includes leap year check"
-    ...
+    #returns the maximum day for a given month. Includes leap year check
+
+
+    #Return the maximum number of days in the given month.
+
+    if month == 2:
+        if leap_year(year):
+            return 29
+        else:
+            return 28
+
+    month_days = {
+        1:31, 3:31, 4:30,
+        5:31, 6:30, 7:31, 8:31,
+        9:30, 10:31, 11:30, 12:31
+    }
+
+    return month_days[month]
 
 def after(date: str) -> str:
     '''
@@ -43,26 +59,15 @@ def after(date: str) -> str:
     year = int(str_year)
     month = int(str_month)
     day = int(str_day)
-    lyear = year % 4
-    if lyear == 0:
-        feb_max = 29 # this is a leap year
-    else:
-        feb_max = 28 # this is not a leap year
+    
+    
 
-    lyear = year % 100
-    if lyear == 0:
-        feb_max = 28 # this is not a leap year
-
-    lyear = year % 400
-    if lyear == 0:
-        feb_max = 29 # this is a leap year
-
-    mon_max = { 1:31, 2:feb_max, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    
 
     tmp_day = day + 1  # next day
 
-    if tmp_day > mon_max[month]:
-        to_day = tmp_day % mon_max[month]  # if tmp_day > this month's max, reset to 1 
+    if tmp_day > mon_max(month, year):
+        to_day = tmp_day % mon_max(month, year)  # if tmp_day > this month's max, reset to 1 
         tmp_month = month + 1
     else:
         to_day = tmp_day
@@ -86,15 +91,71 @@ def usage():
 
 def leap_year(year: int) -> bool:
     "return True if the year is a leap year"
-    ...
+   
+    #Return True if year is a leap year, otherwise False.
+
+    if year % 400 == 0:
+        return True
+
+    elif year % 100 == 0:
+        return False
+
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
 
 def valid_date(date: str) -> bool:
     "check validity of date and return True if valid"
-    ...
+    try:
+        year, month, day = date.split("-")
+        if len(year)!=4 or len(month)!=2 or len(day)!=2:
+            return False
+
+
+        year = int(year)
+        month = int(month)
+        day = int(day)
+
+        if month < 1 or month > 12:
+            return False
+        
+
+        if day < 1 or day > mon_max(month, year):
+            return False
+        return True
+    except:
+        return False
 
 def day_count(start_date: str, stop_date: str) -> int:
     "Loops through range of dates, and returns number of weekend days"
-    ...
+#keep track of number of weekend days found
+    weekend_count = 0
 
+#start checking from the first date in the range
+    current_date = start_date
+#Continue until all dates in the range have been checked 
+    while True:
+        year_str, month_str, day_str = current_date.split("-")
+        year=int(year_str)
+        month=int(month_str)
+        day=int(day_str)
+        weekday = day_of_week(year, month, day)
+#Add a count after every weekday
+        if weekday == "sat" or weekday == "sun":
+            weekend_count +=1
+#Stop at end date
+        if current_date == stop_date:
+            break
+        current_date=after(current_date)
+#return the total number or weekend days found
+    return weekend_count
+
+#check Script
 if __name__ == "__main__":
-    ...
+    print(after("2025-02-26"))
+    print(after("2024-02-28"))
+    print(after("2025-02-28"))
+    print(after("2023-12-31"))
+    print(valid_date("2024-03-29"))
+    print(day_count("2024-01-29", "2024-03-29"))
